6beec6e002f32cf0bc868f9f46fe05dc4f8f2728
rinogodson <rinogodson@gmail.com>
2025-06-07 19:04:47 +0530

    added tag validator

--- filehandler/services.go
+++ filehandler/services.go
@@ -1,3 +1,24 @@
 package filehandler
 
+import (
+	"regexp"
+	"strings"
+)
 
+func TagValidator(tagString string) bool {
+	tagString = strings.TrimSpace(tagString)
+	if tagString == "" {
+		return false
+	}
+	validPattern := regexp.MustCompile(`^(#\w+)(\s+#\w+)*$`)
+	return validPattern.MatchString(tagString)
+}
+
+func ExtractTags(content string) []string {
+	tagPattern := regexp.MustCompile(`#\w+`)
+	tags := tagPattern.FindAllString(content, -1)
+	for i, tag := range tags {
+		tags[i] = strings.TrimPrefix(tag, "#")
+	}
+	return tags
+}
