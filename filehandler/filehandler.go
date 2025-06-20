a8136801b0ac3f48af2b2efed9ee29aaea073edd
rinogodson <rinogodson@gmail.com>
2025-06-20 13:49:40 +0530

    got ai sloped and trynna fix that crap right now (i tried to sort the list according to the created date)

--- filehandler/filehandler.go
+++ filehandler/filehandler.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
+	"time"
 )
 
 func CreateFile(fileName string, content string) {
@@ -28,12 +30,71 @@ func CreateFile(fileName string, content string) {
 	fmt.Fprintln(file, content)
 }
 
+
+func localParseInput(input string) []string {
+	parts := strings.SplitN(input, "~", 2)
+	if len(parts) != 2 {
+		return []string{"", ""}
+	}
+	title := parts[0]
+	dateStr := strings.TrimSuffix(parts[1], ".md")
+
+	dateFields := strings.Fields(dateStr)
+	if len(dateFields) < 5 {
+		fmt.Println("Date string format is invalid")
+		return []string{title, ""}
+	}
+
+	layout := "2006-01-02 15:04:05.999999 -0700"
+	datetimeStr := fmt.Sprintf("%s %s %s", dateFields[0], dateFields[1], dateFields[2])
+	t, err := time.Parse(layout, datetimeStr)
+	if err != nil {
+		fmt.Println("Parse error:", err)
+		return []string{title, ""}
+	}
+
+	day := fmt.Sprintf("%02d", t.Day())
+	month := t.Month().String()
+	year := fmt.Sprintf("%d", t.Year())
+	hour := t.Format("03")
+	minute := t.Format("04")
+	second := t.Format("05")
+	ampm := t.Format("PM")
+	timeStr := fmt.Sprintf("%s:%s:%s", hour, minute, second)
+	timezone := dateFields[3]
+	offset := dateFields[2]
+
+	result := []string{day, month, year, timeStr, ampm, timezone, offset}
+	dateString := strings.Join(result, " ")
+	return []string{title, dateString}
+}
+
 func GetFiles() []os.DirEntry {
 	home, err := os.UserHomeDir()
-	files, err := os.ReadDir(filepath.Join(home, ".dreamshell", "dreams"))
 	if err != nil {
 		panic(err)
 	}
+	dirPath := filepath.Join(home, ".dreamshell", "dreams")
+	files, err := os.ReadDir(dirPath)
+	if err != nil {
+		panic(err)
+	}
+	sort.Slice(files, func(i, j int) bool {
+		filePathI := filepath.Join(dirPath, files[i].Name())
+		filePathJ := filepath.Join(dirPath, files[j].Name())
+		dateStrI := localParseInput(filePathI)[1]
+		dateStrJ := localParseInput(filePathJ)[1]
+		const layout = "02 January 2006 03:04:05 PM MST -0700"
+		createdI, err := time.Parse(layout, dateStrI)
+		if err != nil {
+			panic(err)
+		}
+		createdJ, err := time.Parse(layout, dateStrJ)
+		if err != nil {
+			panic(err)
+		}
+		return createdI.Before(createdJ)
+	})
 	return files
 }
 
