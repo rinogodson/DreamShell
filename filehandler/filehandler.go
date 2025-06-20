12939d5caeff0917ec2f99e844cf2b9cbf0d4889
rinogodson <rinogodson@gmail.com>
2025-06-20 13:56:53 +0530

    it's not sorting the date, leaving it right there for now,
    if you can pls do a PR

--- filehandler/filehandler.go
+++ filehandler/filehandler.go
@@ -14,14 +14,13 @@ func CreateFile(fileName string, content string) {
 	if err != nil {
 		panic(err)
 	}
-
 	dirPath := filepath.Join(home, ".dreamshell", "dreams")
 	err = os.MkdirAll(dirPath, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
-
-	fullPath := filepath.Join(dirPath, fileName+".md")
+	timestamp := time.Now().Format("2006-01-02 15:04:05.999999 -0700")
+	fullPath := filepath.Join(dirPath, fmt.Sprintf("%s~%s.md", fileName, timestamp))
 	file, err := os.Create(fullPath)
 	if err != nil {
 		panic(err)
@@ -30,7 +29,6 @@ func CreateFile(fileName string, content string) {
 	fmt.Fprintln(file, content)
 }
 
-
 func localParseInput(input string) []string {
 	parts := strings.SplitN(input, "~", 2)
 	if len(parts) != 2 {
@@ -38,34 +36,13 @@ func localParseInput(input string) []string {
 	}
 	title := parts[0]
 	dateStr := strings.TrimSuffix(parts[1], ".md")
-
-	dateFields := strings.Fields(dateStr)
-	if len(dateFields) < 5 {
-		fmt.Println("Date string format is invalid")
-		return []string{title, ""}
-	}
-
 	layout := "2006-01-02 15:04:05.999999 -0700"
-	datetimeStr := fmt.Sprintf("%s %s %s", dateFields[0], dateFields[1], dateFields[2])
-	t, err := time.Parse(layout, datetimeStr)
+	t, err := time.Parse(layout, dateStr)
 	if err != nil {
 		fmt.Println("Parse error:", err)
 		return []string{title, ""}
 	}
-
-	day := fmt.Sprintf("%02d", t.Day())
-	month := t.Month().String()
-	year := fmt.Sprintf("%d", t.Year())
-	hour := t.Format("03")
-	minute := t.Format("04")
-	second := t.Format("05")
-	ampm := t.Format("PM")
-	timeStr := fmt.Sprintf("%s:%s:%s", hour, minute, second)
-	timezone := dateFields[3]
-	offset := dateFields[2]
-
-	result := []string{day, month, year, timeStr, ampm, timezone, offset}
-	dateString := strings.Join(result, " ")
+	dateString := t.Format("02 January 2006 03:04:05 PM MST -0700")
 	return []string{title, dateString}
 }
 
@@ -85,13 +62,20 @@ func GetFiles() []os.DirEntry {
 		dateStrI := localParseInput(filePathI)[1]
 		dateStrJ := localParseInput(filePathJ)[1]
 		const layout = "02 January 2006 03:04:05 PM MST -0700"
-		createdI, err := time.Parse(layout, dateStrI)
-		if err != nil {
-			panic(err)
+		var createdI, createdJ time.Time
+		if dateStrI != "" {
+			createdI, err = time.Parse(layout, dateStrI)
+			if err != nil {
+				fmt.Println("Parse error for", filePathI, ":", err)
+				return true
+			}
 		}
-		createdJ, err := time.Parse(layout, dateStrJ)
-		if err != nil {
-			panic(err)
+		if dateStrJ != "" {
+			createdJ, err = time.Parse(layout, dateStrJ)
+			if err != nil {
+				fmt.Println("Parse error for", filePathJ, ":", err)
+				return false
+			}
 		}
 		return createdI.Before(createdJ)
 	})
@@ -111,19 +95,15 @@ func ParseDream(input string) []string {
 	if len(lines) < 2 {
 		return []string{"", "", ""}
 	}
-
 	titleLine := strings.TrimSpace(lines[0])
 	title := strings.TrimPrefix(titleLine, "#")
 	title = strings.TrimSpace(title)
-
 	bodyLines := lines[1 : len(lines)-1]
 	body := strings.Join(bodyLines, "\n")
-
 	tagWords := strings.Fields(lines[len(lines)-1])
 	for i, tag := range tagWords {
 		tagWords[i] = "#" + tag
 	}
 	tags := strings.Join(tagWords, " ")
-
 	return []string{title, body, tags}
 }
